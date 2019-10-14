06c87fce1c2934586952f86bec52b02541b8ae7d
Riley Moore <rilemoore@gmail.com>
2019-10-14 17:24:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def line(array)
+  string = ""
+  count = 0
+  if array.size == 0
+    puts "The line is currently empty."
+  
+  else
+    string << "The line is currently:"
+    while (count < array.size)
+      string << " " << (count + 1).to_s << ". " << array[count]
+      count += 1
+    end
+    puts string
+
+  end
+end
+
+def take_a_number(line_array, name)
+  
+  line_array.push(name)
+  puts "Welcome #{name}. You are number #{line_array.size} in line."
+  
+end
